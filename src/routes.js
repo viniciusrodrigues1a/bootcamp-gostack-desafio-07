360fe202fed40c3481818791fe2b051735680cbf
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-12 20:41:35 -0300

    :sparkles: configurando as rotas da aplicação

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Cart" component={Cart} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
