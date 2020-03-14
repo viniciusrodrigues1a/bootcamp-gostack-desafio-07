a17783c327222b16ef70fbdd164fe82c43e4b0e6
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-14 18:51:28 -0300

    :sparkles: mostrando a logo com um Image

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import { Image } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { Container, Logo, BasketContainer, BasketText } from './styles';
+import logo from '../../assets/images/logo.png';
+
+import { Container, BasketContainer, BasketText } from './styles';
 
 export default function Header() {
   return (
     <Container>
-      <Logo />
+      <Image source={logo} />
       <BasketContainer>
         <Icon name="shopping-basket" size={30} color="#fff" />
         <BasketText>3</BasketText>
