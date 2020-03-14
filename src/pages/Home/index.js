dcdd1c7de1a4c768c2541ba6f28c30c304052e9b
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-13 23:03:34 -0300

    :sparkles: adicionando horizontal flatlist

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,75 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { View } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-import { ProductsList, ProductContainer, ProductImage, ProductName, ProductPrice, AddToCartContainer, AddToCartText } from './styles';
+import img from '../../assets/images/aa.jpg';
 
-export default function Home() {
-  return (
-    <ProductsList>
-      <ProductContainer>
-        <ProductImage source={} />
-        <ProductName>nome....</ProductName>
-        <ProductPrice>R$ 359</ProductPrice>
-        <AddToCartContainer>
-          <Icon name="add-shopping-cart" color="#fff" />
-          <AddToCartText>Adicionar ao carrinho</AddToCartText>
-        </AddToCartContainer>
-      </ProductContainer>
-    </ProductsList>
-  );
+import {
+  ProductsList,
+  ProductContainer,
+  ProductImage,
+  ProductName,
+  ProductPrice,
+  AddToCartButton,
+  AddToCartIconContainer,
+  AddToCartText,
+  AddToCartIconText,
+} from './styles';
+
+export default class Home extends Component {
+  state = {
+    data: [
+      {
+        id: 'asdljmasdljmldas',
+        name: 'Tênis de caminhada leve confortável',
+        price: 'R$ 359,90',
+      },
+      {
+        id: 'asdljmasdasdasdasdljmldas',
+        name: 'coronga de caminhada leve confortável',
+        price: 'R$ 359,90',
+      },
+      {
+        id: 'asdljmasdljmasdasdldas',
+        name: 'chica cunha de caminhada leve confortável',
+        price: 'R$ 359,90',
+      },
+    ],
+  };
+
+  render() {
+    return (
+      <ProductsList
+        // horizontal
+        data={this.state.data}
+        keyExtractor={item => item.id}
+        horizontal
+        renderItem={({ item }) => (
+          <ProductContainer>
+            <ProductImage source={img} />
+            <ProductName>{item.name}</ProductName>
+            <ProductPrice>{item.price}</ProductPrice>
+            <AddToCartButton>
+              <AddToCartIconContainer>
+                <Icon name="add-shopping-cart" size={30} color="#fff" />
+                <AddToCartIconText>0</AddToCartIconText>
+              </AddToCartIconContainer>
+              <AddToCartText>Adicionar</AddToCartText>
+            </AddToCartButton>
+          </ProductContainer>
+        )}
+        ItemSeparatorComponent={() => {
+          return (
+            <View
+              style={{
+                height: '100%',
+                width: 20,
+              }}
+            />
+          );
+        }}
+      />
+    );
+  }
 }
