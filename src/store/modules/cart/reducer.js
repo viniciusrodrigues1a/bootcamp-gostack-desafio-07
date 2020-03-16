2a5aaea661e65bce6a771364b3fe3603f678ef87
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-16 09:58:58 -0300

    :sparkles: utilizando o immer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,7 +1,17 @@
+import produce from 'immer';
+
 export default function(state = [], action) {
   switch (action.type) {
     case '@cart/ADD':
-      return [...state, action.product];
+      return produce(state, draft => {
+        draft.push(action.product);
+      });
+    case '@cart/REMOVE':
+      return produce(state, draft => {
+        const product = draft.find(p => p.id === action.id);
+
+        draft.splice(draft.indexOf(product), 1);
+      });
     default:
       return state;
   }
