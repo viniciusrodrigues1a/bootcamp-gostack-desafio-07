ad8713c6302da5b5dc2831d66af4c359ea21bede
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-16 00:13:15 -0300

    :sparkles: pegando a quantidade de itens do carrinho usando redux

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Image } from 'react-native';
 
+import { connect } from 'react-redux';
+
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import logo from '../../assets/images/logo.png';
 
 import { Container, BasketContainer, BasketText } from './styles';
 
-export default function Header() {
+function Header({ productsInCart }) {
   return (
     <Container>
       <Image source={logo} />
       <BasketContainer>
         <Icon name="shopping-basket" size={30} color="#fff" />
-        <BasketText>3</BasketText>
+        <BasketText>{productsInCart}</BasketText>
       </BasketContainer>
     </Container>
   );
 }
+
+const mapStateToProps = state => ({
+  productsInCart: state.cart.length,
+});
+
+export default connect(mapStateToProps)(Header);
