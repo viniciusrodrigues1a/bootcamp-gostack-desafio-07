a0547d182b9b6f93d30fcbb3d4c7746265a7bf87
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-16 00:13:31 -0300

    :sparkles: configurando react-redux

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import 'react-native-gesture-handler';
 import React from 'react';
 
 import './config/ReactotronConfig';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import Header from './components/Header';
 import Routes from './routes';
 
 export default function App() {
   return (
     <>
-      <Header />
-      <Routes />
+      <Provider store={store}>
+        <Header />
+        <Routes />
+      </Provider>
     </>
   );
 }
