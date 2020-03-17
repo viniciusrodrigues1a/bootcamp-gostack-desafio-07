b69f7869ec5921bd64a7439be6e335f6fcefe361
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-17 00:34:09 -0300

    ignorando warn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './src';
 import { name as appName } from './app.json';
+
+YellowBox.ignoreWarnings(['VirtualizedLists should never be nested']);
 
 AppRegistry.registerComponent(appName, () => App);
