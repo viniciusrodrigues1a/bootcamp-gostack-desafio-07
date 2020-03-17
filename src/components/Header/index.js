08cc9bb31bc89c2a318ec00762339c36d63a715d
viniciusrodrigues1a <viniciusrodrigues.aro@gmail.com>
2020-03-17 00:40:33 -0300

    :sparkles: mostrando seta de voltar no Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image } from 'react-native';
-
 import { connect } from 'react-redux';
+import { useNavigation } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -10,8 +10,18 @@ import logo from '../../assets/images/logo.png';
 import { Container, BasketContainer, BasketText } from './styles';
 
 function Header({ productsInCart }) {
+  const navigation = useNavigation();
+
   return (
     <Container>
+      {navigation.canGoBack() && (
+        <Icon
+          name="arrow-back"
+          color="#fff"
+          size={28}
+          onPress={() => navigation.navigate('Home')}
+        />
+      )}
       <Image source={logo} />
       <BasketContainer>
         <Icon name="shopping-basket" size={30} color="#fff" />
